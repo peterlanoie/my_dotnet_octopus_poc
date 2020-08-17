8dcdb5ef8a318c989e25fc6e607a9f4f3f02f412
Alex Yates <Alex-Yates@users.noreply.github.com>
2020-08-17 12:43:07 +0100

    Update setup_dotnet_core.ps1

--- Infrastructure/UserDataDownloads/setup_dotnet_core.ps1
+++ Infrastructure/UserDataDownloads/setup_dotnet_core.ps1
@@ -10,10 +10,10 @@ function Download-File
   $downloader.DownloadFile($url, $saveAs)
 }
 
-# Installing ASP.NET Core 2.2 Runtime (v2.2.8) - Windows Hosting Bundle Installer so that we can deploy ,.NET Core v2.2 websites
+# Installing ASP.NET Core 2.0 Runtime (v2.0.3) - Windows Hosting Bundle Installer so that we can deploy ,.NET Core v2.0 websites
 
-$dotnetUrl = "https://download.visualstudio.microsoft.com/download/pr/ba001109-03c6-45ef-832c-c4dbfdb36e00/e3413f9e47e13f1e4b1b9cf2998bc613/dotnet-hosting-2.2.8-win.exe"
-$dotnetHostingBundleInstaller = "$startupDir/dotnet-hosting-2.2.8-win.exe"
+$dotnetUrl = "https://download.microsoft.com/download/5/C/1/5C190037-632B-443D-842D-39085F02E1E8/DotNetCore.2.0.3-WindowsHosting.exe"
+$dotnetHostingBundleInstaller = "$startupDir/DotNetCore.2.0.3-WindowsHosting.exe"
 
 if ((test-path $dotnetHostingBundleInstaller) -ne $true) {
     Download-File -url $dotnetUrl -SaveAs $dotnetHostingBundleInstaller 
@@ -22,7 +22,7 @@ else {
     Write-Output "  dotnet core hosting bundle already downloaded to $dotnetHostingBundleInstaller"
 }
 
-Write-Output "  Installing ASP.NET Core 2.2 Runtime (v2.2.8) - Windows Hosting Bundle Installer."
+Write-Output "  Installing ASP.NET Core 2.0 Runtime (v2.0.3) - Windows Hosting Bundle Installer."
 $args = New-Object -TypeName System.Collections.Generic.List[System.String]
 $args.Add("/quiet")
 $args.Add("/install")
