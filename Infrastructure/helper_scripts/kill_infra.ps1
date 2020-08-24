78670de632c29070a3ec7ce6a2c2b144cfaaa6e1
Alex Yates <alex.yates@dlmconsultants.com>
2020-08-24 13:44:29 +0100

    typo

--- Infrastructure/helper_scripts/kill_infra.ps1
+++ Infrastructure/helper_scripts/kill_infra.ps1
@@ -141,5 +141,5 @@ if (($numOfInstancesToKill -ne 0) -or ($numOfTargetsToKill -ne 0)){
     Write-Error "Not all the EC2 instances / Octopus target manchines have been successfully killed."
 }
 else {
-    Write-Host "SUCCESS! All EC2 instances and Octopus target manchines for project $project in environment $octoEnvName have been killed."
+    Write-Host "SUCCESS! All EC2 instances and Octopus target machines for project $project in environment $octoEnvName have been killed."
 }
