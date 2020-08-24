4d9f8619f2f1c61edb72b5048fd2c50e355917df
Alex Yates <alex.yates@dlmconsultants.com>
2020-08-24 12:31:46 +0100

    fixing the AWS install race condition bug

--- Infrastructure/helper_scripts/install_AWS_tools.ps1
+++ Infrastructure/helper_scripts/install_AWS_tools.ps1
@@ -1,5 +1,49 @@
 $ErrorActionPreference = "Stop"  
 
+# Check to see if holding file exists - implying that another process is already installing AWS Tools
+$holdingFile = "$PSScriptRoot/holdingfile.txt"
+$warningTime = 90 # seconds
+$warningGiven = $false
+$timeoutTime = 120 # seconds
+if (test-path $holdingFile){
+    $holdingFileText = Get-Content -Path $holdingFile -Raw
+    Write-Output "    $holdingFileText"
+    $AwsBeingInstalled = $true
+    $stopwatch =  [system.diagnostics.stopwatch]::StartNew()
+    while ($AwsBeingInstalled){
+        Start-Sleep -s 5
+        $time = [Math]::Floor([decimal]($stopwatch.Elapsed.TotalSeconds))
+        if (-not (test-path $holdingFile)){
+            $AwsBeingInstalled = $false
+            Write-Output "   Looks like the AWS Tools install should be finished now."
+            Write-Output "   Verifying that AWS Tools is installed correctly..."
+        }
+        if ($AwsBeingInstalled){
+            Write-Output "      $time seconds: AWS Tools still being installed..."
+        }
+        if (($time -ge $warningTime) -and (-not $warningGiven)){
+            Write-Warning "Installing AWS Tools normally only takes about 70 seconds."
+            $warningGiven = $true
+        }
+        if ($time -ge $timeoutTime){
+            Write-Error "Timed out at $time seconds."
+        }
+    }
+}
+
+# Create holding file to stop any other runbooks from installing AWS Tools at the same time
+$RunbookRunId = "[RunbookRunId unknown]"
+try {
+    $RunbookRunId = $OctopusParameters["Octopus.RunbookRun.Id"]
+}
+catch {
+    Write-Warning "Failed to detect Octopus.RunbookRun.Id from Octopus system variables."
+}
+$startTime = Get-Date
+$holdingFileText = "Runbook $RunbookRunId installing AWS tools at: $startTime"
+$holdingFileText | out-file $holdingFile
+
+# Installing AWS Tools
 $Installedmodules = Get-InstalledModule
 
 if ($Installedmodules.name -contains "AWS.Tools.Common"){
@@ -28,3 +72,8 @@ else {
     Write-Output "    Installing AWS.Tools.IdentityManagement..."
     Install-Module AWS.Tools.IdentityManagement -Force
 }
+
+Write-Output "    AWS Tools is set up and ready to use."
+
+# Delete holding file
+Remove-Item $holdingFile
