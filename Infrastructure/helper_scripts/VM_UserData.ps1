ebccb2013b462f1e5d5b8e3802d18f68f09a7a18
Alex Yates <alex.yates@dlmconsultants.com>
2020-09-23 10:44:17 +0100

    Fixing the userdata script

--- Infrastructure/helper_scripts/VM_UserData.ps1
+++ Infrastructure/helper_scripts/VM_UserData.ps1
@@ -2,6 +2,8 @@
 $ErrorActionPreference = "stop"
 
 $startupDir = "C:\Startup"
+$scriptsDir = "scripts"
+
 if ((test-path $startupDir) -ne $true) {
   New-Item -ItemType "Directory" -Path $startupDir
 }
@@ -13,41 +15,43 @@ $log = ".\StartupLog.txt"
 Write-Output " Creating log file at $log"
 Start-Transcript -path $log -append
 
+Set-Location $startupDir
+
+if ((test-path $scriptsDir) -ne $true) {
+  New-Item -ItemType "Directory" -Path $scriptsDir
+}
+
+Set-Location $scriptsDir
+
 Function Get-Script{
   param (
     [Parameter(Mandatory=$true)][string]$script,
     [string]$owner = "__REPOOWNER__",
     [string]$repo = "__REPONAME__",
     [string]$branch = "main",
-    [string]$path = "Infrastructure\UserDataDownloads",
-    [string]$outDir = "scripts",
-    [string]$outFile = ".\scripts\$script"
+    [string]$path = "Infrastructure\UserDataDownloads"
   )
-  if ((test-path $path) -ne $true) {
-    Write-Output "  Creating directory $startupDir\$outDir"
-    New-Item -ItemType "Directory" -Path $outDir
-  }
   $uri = "https://raw.githubusercontent.com/$owner/$repo/$branch/$path/$script"
   Write-Output "Downloading $script"
   Write-Output "  from: $uri"
-  Write-Output "  to: $outFile"
-  Invoke-WebRequest -Uri $uri -OutFile $outFile -Verbose
+  Write-Output "  to: .\$script"
+  Invoke-WebRequest -Uri $uri -OutFile ".\$script" -Verbose
 }
 
 Write-Output "*"
 Get-Script -script "setup_users.ps1"
-Write-Output "Executing ./$outDir/setup_users.ps1"
-./$outDir/setup_users.ps1
+Write-Output "Executing ./setup_users.ps1"
+./setup_users.ps1
 
 Write-Output "*"
 Get-Script -script "setup_iis.ps1"
-Write-Output "Executing ./$outDir/setup_iis.ps1"
-./$outDir/setup_iis.ps1
+Write-Output "Executing ./setup_iis.ps1"
+./setup_iis.ps1
 
 Write-Output "*"
 Get-Script -script "setup_dotnet_core.ps1"
-Write-Output "Executing ./$outDir/setup_dotnet_core.ps1"
-./$outDir/setup_dotnet_core.ps1
+Write-Output "Executing ./setup_dotnet_core.ps1"
+./setup_dotnet_core.ps1
 
 <# DEPLOY TENTACLE
 $octopusServerUrl = "__OCTOPUSURL__"
@@ -55,8 +59,8 @@ $registerInEnvironments = "__ENV__"
 
 Write-Output "*"
 Get-Script -script "install_tentacle.ps1"
-Write-Output "Executing ./$outDir/install_tentacle.ps1 -octopusServerUrl $octopusServerUrl -registerInEnvironments $registerInEnvironments"
-./$outDir/install_tentacle.ps1 -octopusServerUrl $octopusServerUrl -registerInEnvironments $registerInEnvironments
+Write-Output "Executing ./install_tentacle.ps1 -octopusServerUrl $octopusServerUrl -registerInEnvironments $registerInEnvironments"
+./install_tentacle.ps1 -octopusServerUrl $octopusServerUrl -registerInEnvironments $registerInEnvironments
 DEPLOY TENTACLE #>
 
 Write-Output "VM_UserData startup script completed..."
