7857e037ec63db33a18461465db3afcaaef070a7
Alex Yates <alex.yates@dlmconsultants.com>
2020-09-25 12:12:06 +0100

    making add_role_to_profile.ps1 rerunnable

--- Infrastructure/helper_scripts/add_role_to_profile.ps1
+++ Infrastructure/helper_scripts/add_role_to_profile.ps1
@@ -1,2 +1,20 @@
-﻿New-IAMInstanceProfile -InstanceProfileName RandomQuotes
+﻿try {
+    Remove-IAMRoleFromInstanceProfile -InstanceProfileName RandomQuotes -RoleName SecretsManager -Force
+    Write-Output "Existing SecretsManager role removed from profile RandomQuotes."
+}
+catch {
+    Write-Output "SecretsManager role is not already added to profile RandomQuotes"
+}
+try {
+    Remove-IAMInstanceProfile -InstanceProfileName RandomQuotes -Force
+    Write-Output "Removed existing profile RandomQuotes."
+}
+catch {
+    Write-Output "Profile RandomQuotes does not already exist."
+}
+
+Write-Output "Creating new profile: RandomQuotes"
+New-IAMInstanceProfile -InstanceProfileName RandomQuotes
+
+Write-Output "Adding SecretsManager role to profile RandomQuotes."
 Add-IAMRoleToInstanceProfile -InstanceProfileName RandomQuotes -RoleName SecretsManager
