248c7ae3fa591a043f1857f45ac674447b543662
Alex Yates <alex.yates@dlmconsultants.com>
2021-01-05 10:38:43 +0000

    stub code for actual runbook scrip[t

---
+++ Infrastructure/hard_reset-kill_everything.ps1
@@ -0,0 +1,78 @@
+param(
+    $awsAccessKey = "",
+    $awsSecretKey = "",
+    $defaulAwsRegion = "eu-west-1" # Other carbon neutral regions are listed here: https://aws.amazon.com/about-aws/sustainability/
+)
+
+$ErrorActionPreference = "Stop"  
+
+# Setting default values for parameters
+
+$missingParams = @()
+
+if ($awsAccessKey -like ""){
+    try {
+        $awsAccessKey = $OctopusParameters["AWS_ACCOUNT.AccessKey"]
+        Write-Output "Found value for awsAccessKey from Octopus variables." 
+    }
+    catch {
+        $missingParams = $missingParams + "-awsAccessKey"
+    }
+}
+
+if ($awsSecretKey -like ""){
+    try {
+        $awsSecretKey = $OctopusParameters["AWS_ACCOUNT.SecretKey"]
+        Write-Output "Found value for awsSecretKey from Octopus variables." 
+    }
+    catch {
+        $missingParams = $missingParams + "-awsSecretKey"
+    }
+}
+
+if ($missingParams.Count -gt 0){
+    $errorMessage = "Missing the following parameters: "
+    foreach ($param in $missingParams) {
+        $errorMessage += "$param, "
+    }
+    Write-Error $errorMessage
+}
+
+Write-Output "  Execution root dir: $PSScriptRoot"
+Write-Output "*"
+
+# Install AWS tools
+Write-Output "Executing .\helper_scripts\install_AWS_tools.ps1..."
+Write-Output "  (No parameters)"
+& $PSScriptRoot\helper_scripts\install_AWS_tools.ps1
+Write-Output "*"
+
+# Configure your default profile
+Write-Output "Executing .\helper_scripts\configure_default_aws_profile.ps1..."
+Write-Output "  Parameters: -AwsAccessKey $awsAccessKey -AwsSecretKey *** -DefaulAwsRegion $defaulAwsRegion"
+& $PSScriptRoot\helper_scripts\configure_default_aws_profile.ps1 -AwsAccessKey $awsAccessKey -AwsSecretKey $awsSecretKey -DefaulAwsRegion $defaulAwsRegion
+Write-Output "*"
+
+# Delete the RandomQuotes Instances
+Write-Output "Executing .\helper_scripts\delete_all_randomquotes_instances.ps1..."
+Write-Output "  (No parameters)"
+& $PSScriptRoot\helper_scripts\delete_all_randomquotes_instances.ps1 
+Write-Output "*"
+
+# Delete the RandomQuotes Tentacles
+Write-Output "Executing .\helper_scripts\delete_all_randomquotes_tentacles.ps1..."
+Write-Output "  (No parameters)"
+& $PSScriptRoot\helper_scripts\delete_all_randomquotes_tentacles.ps1 
+Write-Output "*"
+
+# Delete the RandomQuotes Keypair
+Write-Output "Executing .\helper_scripts\delete_randomquotes_ keypair.ps1..."
+Write-Output "  (No parameters)"
+& $PSScriptRoot\helper_scripts\delete_randomquotes_keypair.ps1 
+Write-Output "*"
+
+# Delete the RandomQuotes Security Group
+Write-Output "Executing .\helper_scripts\delete_randomquotes_security_group.ps1..."
+Write-Output "  (No parameters)"
+& $PSScriptRoot\helper_scripts\delete_all_randomquotes_security_group.ps1 
+Write-Output "*"
