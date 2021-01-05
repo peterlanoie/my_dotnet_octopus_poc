c8425cdc7367c1f5c9bbe5b5dd1c1fbb327c928f
Alex Yates <alex.yates@dlmconsultants.com>
2021-01-05 14:58:24 +0000

    Code to delete security group and stub code for removing IAM role.

---
+++ Infrastructure/helper_scripts/delete_randomquotes_iam_role.ps1
@@ -0,0 +1,3 @@
+Write-Warning "This script is not yet implemented! See logs for required actions."
+Write-Output "To do: Remove IAM role manually."
+Write-Output "To do: write the code for this script."
---
+++ Infrastructure/helper_scripts/delete_randomquotes_securitygroup.ps1
@@ -0,0 +1,40 @@
+param(
+    $securityGroupName = "RandomQuotes"
+)
+
+$ErrorActionPreference = "Stop"  
+
+# Helper function to see if security group exists
+function Test-SecurityGroup {
+    param (
+        $name
+    )
+
+    try {
+        Get-SecurityGroup -GroupName $name | out-null
+        return $true
+    }
+    catch {
+        return $false
+    }
+}
+
+# Deleting security group
+$secGroupExistsBefore = Test-SecurityGroup $securityGroupName
+if ($secGroupExistsBefore) {
+    Write-Output "    Security group exists in EC2."
+    Write-Output "    Deleting security group: $securityGroupName"
+    Remove-SecurityGroup -GroupName $securityGroupName -Force
+}
+else {
+    "    $securityGroupName security group does not exist in EC2. No need to delete it."
+}
+
+# Verifying security group deleted
+$secGroupExistsAfter = Test-SecurityGroup $securityGroupName
+if ($secGroupExistsBefore -and $secGroupExistsAfter) {
+    Write-Error "    Failed to delete security group: $securityGroupName"
+}
+if ($secGroupExistsBefore -and -not $secGroupExistsAfter) {
+    Write-Output "    Security group successfully deleted."
+}
