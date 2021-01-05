2db74828a5c953bed21b2492f9f1940ff6e8c66a
Alex Yates <alex.yates@dlmconsultants.com>
2021-01-05 16:58:38 +0000

    Oops, used = instead of -eq. Fixed now

--- Infrastructure/helper_scripts/delete_randomquotes_securitygroup.ps1
+++ Infrastructure/helper_scripts/delete_randomquotes_securitygroup.ps1
@@ -23,21 +23,21 @@ $secGroupExistsBefore = Test-SecurityGroup $securityGroupName
 if ($secGroupExistsBefore) {
     Write-Output "    Security group exists in EC2."
     Write-Output "    Deleting security group: $securityGroupName"
-    $attempts = 12
+    $attempts = 20
     $waitTime = 5
     for ($i -eq 1; $i -le $attempts; $i++){
+        Write-Output "      Attempt $i / $attempts to delete security group: $securityGroupName"
         try {
-            Write-Output "      Attempt $i / $attempts to delete security group: $securityGroupName"
             Remove-EC2SecurityGroup -GroupName $securityGroupName -Force
             break
         }
         catch {
             Write-Output "        Failed to remove security group. Error was:"
-            if ($i = 1) {
-                Write-Output "        (We probably need to wait about 20-30 seconds for the instances to shut down.)"
-            }
             $lastError = $Error[0]
             Write-Output "          $lastError"
+            if ($i -eq 1) {
+                Write-Output "        (We probably need to wait about a minute for the instances to shut down.)"
+            }
             if ($i -lt 10) {
                 Write-Output "        Waiting $waitTime seconds then trying again."
                 Start-Sleep -s $waitTime
@@ -45,7 +45,6 @@ if ($secGroupExistsBefore) {
             else {
                 Write-Error "Failed to delete security group. Ran out of attempts. If it was dependencies, ensure all instances are terminated, then try again."
             }
-            
         }
     }
 }
