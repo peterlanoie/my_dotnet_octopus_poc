a13d619148e3967f7d151c28b6faa08d13d65ee0
Alex Yates <alex.yates@dlmconsultants.com>
2021-01-05 11:28:59 +0000

    adding code to delete keypair

--- Infrastructure/helper_scripts/delete_randomquotes_ keypair.ps1.ps1
+++ Infrastructure/helper_scripts/delete_randomquotes_ keypair.ps1.ps1
@@ -1,1 +1,40 @@
-Write-Warning "To do: Implement this script"
+param(
+    $keyPairName = "RandomQuotes"
+)
+
+$ErrorActionPreference = "Stop"  
+
+# Helper function to see if keypair exists
+function Test-KeyPair {
+    param (
+        $name
+    )
+
+    try {
+        Get-EC2KeyPair -KeyName $name | out-null
+        return $true
+    }
+    catch {
+        return $false
+    }
+}
+
+# Deleting keypair
+$keyPairExistsBefore = Test-KeyPair $keyPairName
+if ($keyPairExistsBefore) {
+    Write-Output "    Keypair exists in EC2."
+    Write-Output "    Deleting keypair: $keyPairName"
+    Remove-EC2KeyPair -KeyName $keyPairName -Force
+}
+else {
+    "    $keyPairName does not exist in EC2. No need to delete it."
+}
+
+# Verifying keypair deleted
+$keyPairExistsAfter = Test-KeyPair $keyPairName
+if ($keyPairExistsBefore -and $keyPairExistsAfter) {
+    Write-Error "    Failed to delete keypair: $keyPairName"
+}
+if ($keyPairExistsBefore -and -not $keyPairExistsAfter) {
+    Write-Output "    Keypair successfully deleted."
+}
