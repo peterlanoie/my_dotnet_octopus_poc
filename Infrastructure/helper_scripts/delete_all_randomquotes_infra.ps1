40b233de93803ba00b5a3ef62a6006e80fd499b7
Alex Yates <alex.yates@dlmconsultants.com>
2021-01-05 10:59:06 +0000

    Addiong first attempt at code for deleting all envs

--- Infrastructure/helper_scripts/delete_all_randomquotes_infra.ps1
+++ Infrastructure/helper_scripts/delete_all_randomquotes_infra.ps1
@@ -1,1 +1,53 @@
-Write-Warning "To do: Implement this script"
+param(
+    $project = "",
+    $octoUrl = "",
+    $octoApiKey = "",
+    $spaceId = "Spaces-1" # If you are using the non-default space you will need to update this
+)
+
+$ErrorActionPreference = "Stop"
+
+# Setting default values for parameters
+$missingParams = @()
+
+if ($octoApiKey -like ""){
+    try {
+        $octoApiKey = $OctopusParameters["API_KEY"]
+        Write-Output "    Found value for octoApiKey from Octopus variables." 
+    }
+    catch {
+        $missingParams = $missingParams + "-octoApiKey"
+    }
+}
+
+if ($octoUrl -like ""){
+    try {
+        $octoUrl = $OctopusParameters["Octopus.Web.ServerUri"]
+        Write-Output "    Found value for octoUrl from Octopus variables: $octoUrl" 
+    }
+    catch {
+        $missingParams = $missingParams + "-octoUrl"
+    }
+}
+
+if ($missingParams.Count -gt 0){
+    $errorMessage = "Missing the following parameters: "
+    foreach ($param in $missingParams) {
+        $errorMessage += "$param, "
+    }
+    Write-Error $errorMessage
+}
+
+# API header for Octopus Deploy
+$octoApiHeader = @{ "X-Octopus-ApiKey" = $octoApiKey }
+
+# Finding all the environments
+$environments = (Invoke-WebRequest "$octoUrl/api/environments" -Headers $octoApiHeader -UseBasicParsing).content | ConvertFrom-Json
+
+# Deleting all the VMs and tentacles
+ForEach-Object $envName in $environments {
+    Write-Output "Executing $PSScriptRoot\kill_infra.ps1..."
+    Write-Output "  Parameters: -octoEnvName $envName"
+    & $PSScriptRoot\helper_scripts\kill_infra.ps1 -octoEnvName $envName
+    Write-Output "*"
+}
