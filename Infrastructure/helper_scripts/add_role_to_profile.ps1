363399c48495113249ada5097ec1f49f260bc783
Alex Yates <alex.yates@dlmconsultants.com>
2021-01-07 09:47:50 +0000

    slicker logs

--- Infrastructure/helper_scripts/add_role_to_profile.ps1
+++ Infrastructure/helper_scripts/add_role_to_profile.ps1
@@ -1,20 +1,20 @@
 ﻿try {
     Remove-IAMRoleFromInstanceProfile -InstanceProfileName RandomQuotes -RoleName SecretsManager -Force
-    Write-Output "Existing SecretsManager role removed from profile RandomQuotes."
+    Write-Output "    Existing SecretsManager role removed from profile RandomQuotes."
 }
 catch {
-    Write-Output "SecretsManager role is not already added to profile RandomQuotes"
+    Write-Output "    SecretsManager role is not already added to profile RandomQuotes"
 }
 try {
     Remove-IAMInstanceProfile -InstanceProfileName RandomQuotes -Force
-    Write-Output "Removed existing profile RandomQuotes."
+    Write-Output "    Removed existing profile RandomQuotes."
 }
 catch {
-    Write-Output "Profile RandomQuotes does not already exist."
+    Write-Output "    Profile RandomQuotes does not already exist."
 }
 
-Write-Output "Creating new profile: RandomQuotes"
+Write-Output "    Creating new profile: RandomQuotes"
 New-IAMInstanceProfile -InstanceProfileName RandomQuotes
 
-Write-Output "Adding SecretsManager role to profile RandomQuotes."
+Write-Output "    Adding SecretsManager role to profile RandomQuotes."
 Add-IAMRoleToInstanceProfile -InstanceProfileName RandomQuotes -RoleName SecretsManager
