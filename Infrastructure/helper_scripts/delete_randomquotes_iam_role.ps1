56e7f4078141a69b9ab3bbfcea79803fe276186f
Alex Yates <alex.yates@dlmconsultants.com>
2021-01-07 15:19:25 +0000

    Hopefully this will complete the hard reset

--- Infrastructure/helper_scripts/delete_randomquotes_iam_role.ps1
+++ Infrastructure/helper_scripts/delete_randomquotes_iam_role.ps1
@@ -15,16 +15,24 @@ if ($roleExists) {
     Write-Output "      Removing policy from SecretsManager role."
     Get-IAMAttachedRolePolicyList -RoleName SecretsManager | Unregister-IAMRolePolicy -RoleName SecretsManager
     
+    Write-Output "      Attempting to remove SecretsManager role RendomQuotes profile."
+    try {
+        Remove-IAMRoleFromInstanceProfile -InstanceProfileName RandomQuotes -RoleName SecretsManager -Force
+        Write-Output "      Removed role SecretsManager from profile RandomQuotes"
+    }
+    catch {
+        Write-Output "      Role SecretsManager was not added to profile RandomQuotes"
+    }
+    
     Write-Output "      Removing SecretsManager role."
     Remove-IAMRole -RoleName SecretsManager -Force    
 }
 
 # If RandomQuotes profile exists, delete it.
-Write-Output "      Attempting to remove RendomQuotes profile."
 try {
     Remove-IAMInstanceProfile -InstanceProfileName RandomQuotes -Force
-    Write-Output "        Removed profile RandomQuotes."
+    Write-Output "      Removed profile RandomQuotes."
 }
 catch {
-    Write-Output "        Profile RandomQuotes didn't exist."
+    Write-Output "      Profile RandomQuotes does not exist."
 }
